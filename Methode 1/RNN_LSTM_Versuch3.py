6617fbb2f5108ef1c9c992c3d0fbb29c2c19424c
Christian <christian.haeusler@mail.ru>
2017-06-09 10:56:17 +0200

    Add early stopping

--- Methode 1/RNN_LSTM_Versuch3.py
+++ Methode 1/RNN_LSTM_Versuch3.py
@@ -59,10 +59,13 @@ X_test = X_test[:, :, None]
 #y_test = y_test[:, :, newaxis]
 
 
-adam = optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+adam = optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, 
+                       decay=0.0)
 sgd = optimizers.SGD(lr=0.01, momentum=0.0, decay=0.05, nesterov=False)
 reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
                               patience=10, min_lr=0.0002)
+early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=3, 
+                                         mode='auto')
 
 print(len(X_train), 'train sequences')
 print(len(X_test), 'test sequences')
@@ -81,7 +84,7 @@ def lstm():
     model.add(LSTM(50,
                    return_sequences=False,
                    stateful=True, activation='relu'))
-    model.add(Dense(1, activation='linear'))
+    model.add(LSTM(1, activation='linear'))
     model.compile(loss='mse', optimizer=adam)  
     return model
 
@@ -90,7 +93,7 @@ model = lstm()
 
 print('Train...')
 his = model.fit(X_train, y_train, batch_size=batch_size, epochs=50, verbose=1, 
-      callbacks=[history, reduce_lr], validation_split=0.0, 
+      callbacks=[history, reduce_lr, early_stopping], validation_split=0.0, 
       validation_data=None, class_weight=None, 
       sample_weight=None, initial_epoch=0, shuffle=True)
 
