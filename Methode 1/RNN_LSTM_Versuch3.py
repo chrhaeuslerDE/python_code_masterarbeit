cb3c5f1d0f01fca61fb0a486085577733083e1d6
Christian <christian.haeusler@mail.ru>
2017-06-13 12:01:43 +0200

    test

--- Methode 1/RNN_LSTM_Versuch3.py
+++ Methode 1/RNN_LSTM_Versuch3.py
@@ -16,6 +16,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from keras import callbacks
 
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
@@ -29,35 +30,24 @@ def create_dataset(dataset, look_back=1):
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
+history = callbacks.History()
+
 # load the dataset
 print('Loading data...')
 dataframeTrain = pandas.DataFrame(pandas.read_csv('train.txt', sep = ' '))
 dataframeTest = pandas.DataFrame(pandas.read_csv('test.txt', sep = ' '))
 datasetTr = dataframeTrain.values
 datasetTr = datasetTr.astype('float32')
+trainY = datasetTr[0:45000,27]
 datasetTe = dataframeTest.values
 datasetTe = datasetTe.astype('float32')
+testY = datasetTe[53:209,27]
 
 
 # normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
-datasetTr = scaler.fit_transform(datasetTr)
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-datasetTe = scaler.fit_transform(datasetTe)
-
-"""
-# split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-"""
-# split into input (X) and output (Y) variables
-trainX = datasetTr[0:45000,6:26]
-trainY = datasetTr[0:45000,27]
-testX = datasetTe[53:209,6:26] #Sequence 2
-testY = datasetTe[53:209,27]
-
+trainX = scaler.fit_transform(datasetTr[0:45000,6:26])
+testX = scaler.fit_transform(datasetTe[53:209,6:26])
 
 # reshape input to be [samples, time steps, features]
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
@@ -68,7 +58,8 @@ model = Sequential()
 model.add(LSTM(4, input_shape=(1, 20)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=100, verbose=1)
+model.fit(trainX, trainY, epochs=10, batch_size=100, verbose=1, \
+          validation_split=0.05)
 
 # make predictions
 trainPredict = model.predict(trainX)
@@ -86,11 +77,28 @@ print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 
+
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
 trainPredictPlot[:, :] = numpy.nan
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
                  
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
+
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
 # shift test predictions for plotting
 testPredictPlot = numpy.empty_like(dataset)
 testPredictPlot[:, :] = numpy.nan
