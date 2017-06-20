25072b78ba034d8c69ce36350b50e62ba0934ea8
Christian <christian.haeusler@mail.ru>
2017-06-20 15:53:34 +0200

    Add StandardScaler

--- Methode 1/RNN_LSTM_Versuch3.py
+++ Methode 1/RNN_LSTM_Versuch3.py
@@ -10,15 +10,24 @@ Created on Tue Jun 13 09:56:23 2017
 import numpy
 import matplotlib.pyplot as plt
 from pandas import read_csv
-import math, pandas
+import pandas
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
+from keras import callbacks, optimizers
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import mean_squared_error
 
 # fix random seed for reproducibility
 numpy.random.seed(7)
+
+history = callbacks.History()
+
+adam = optimizers.Adam(lr=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                              patience=5, min_lr=0.0001)
+early_stopping = callbacks.EarlyStopping(monitor='val_loss', patience=30, 
+                                         mode='auto')
 
 # load the dataset
 print('Loading data...')
@@ -29,18 +38,24 @@ datasetTr = datasetTr.astype('float32')
 datasetTe = dataframeTest.values
 datasetTe = datasetTe.astype('float32')
 
-"""
-# split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-"""
+
 # split into input (X) and output (Y) variables
 trainX = datasetTr[0:45000,6:26]
 trainY = datasetTr[0:45000,27]
-testX = datasetTe[53:209,6:26] #Sequence 2
-testY = datasetTe[53:209,27]
+testX = datasetTe[210:325,6:26] #Sequence 3
+testY = datasetTe[210:325,27]
 
+#Trainingsdatensatz Standardisieren
+sc = StandardScaler()
+sc.fit(trainX)
+trainX = sc.transform(trainX)
+
+#Testdatensatz Standardisieren
+sc = StandardScaler()
+sc.fit(testX)
+testX = sc.transform(testX)
+
+"""
 # normalize the dataset
 scalerTrainX = MinMaxScaler(feature_range=(0, 1))
 trainX = scalerTrainX.fit_transform(trainX)
@@ -53,11 +68,14 @@ testX = scalerTestX.fit_transform(testX)
 
 scalerTestY = MinMaxScaler(feature_range=(0, 1))
 testY = scalerTestY.fit_transform(testY)
-
+"""
 
 # reshape input to be [samples, time steps, features]
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+#Respahe Y to (samples,1)
+trainY = numpy.reshape(trainY, (len(trainY),1))
+
 
 # create and fit the LSTM network
 def build_model(layers):
@@ -70,6 +88,12 @@ def build_model(layers):
     model.add(Dropout(0.2))
 
     model.add(LSTM(
+    layers[1],
+    input_shape=(1,20),
+    return_sequences=True))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(
         layers[2],
         return_sequences=False))
     model.add(Dropout(0.2))
@@ -78,41 +102,27 @@ def build_model(layers):
             layers[3]))
     model.add(Activation("linear"))
 
-    model.compile(loss="mse", optimizer="rmsprop")
+    model.compile(loss="mse", optimizer="adam")
     return model
 
 
 model = build_model([1, 50, 100, 1])
 
-model.fit(trainX, trainY, epochs=1000, batch_size=500,validation_split=0.05 ,\
-          verbose=1)
+model.fit(trainX, trainY, epochs=250, batch_size=50,validation_split=0.05 ,\
+          verbose=1, callbacks=[history, reduce_lr, early_stopping])
 
 # make predictions
 testPredict = model.predict(testX)
 
-# invert predictions
-testPredict = scalerTestY.inverse_transform(testPredict)
-testY = scalerTestY.inverse_transform([testY])
-"""
-# calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-print('Test Score: %.2f RMSE' % (testScore))
-
-
-# shift train predictions for plotting
-trainPredictPlot = numpy.empty_like(dataset)
-trainPredictPlot[:, :] = numpy.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-                 
-# shift test predictions for plotting
-testPredictPlot = numpy.empty_like(dataset)
-testPredictPlot[:, :] = numpy.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-"""
 # plot baseline and predictions
 plt.plot(testPredict)
 testY = datasetTe[53:209,27]
 plt.plot(testY)
 plt.show()
+
+"""
+#PLot the history of the training
+plt.plot(history)
+plt.title("History of Epochs")
+plt.show()
+"""
